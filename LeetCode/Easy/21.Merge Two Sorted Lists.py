026069a78844815d7f05ef31a817a4baeac77cd1
Anthony <atsui4688@gmail.com>
2020-03-29 18:51:34 -0400

    possibleSums from CodeSignal

--- LeetCode/Easy/21.Merge Two Sorted Lists.py
+++ LeetCode/Easy/21.Merge Two Sorted Lists.py
@@ -14,8 +14,7 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-#Time complexity: O(N + M) where n & m are the lengths of l1 and l2, Space Complexity: O(M+N) since we are creating a new list that merges both linked lists
-
+#Another Iterative Solution 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
@@ -23,30 +22,52 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        newHead = ListNode(None)
-        curr = newHead
-        while l1 or l2:
-            if not l1:
-                curr.next = l2
-                break
-            if not l2:
-                curr.next = l1
-                break
-            v1, v2 = l1.val, l2.val
-            
-            if v1 > v2:
-                curr.next = ListNode(v2)
-                curr = curr.next
-                l2 = l2.next
-            elif v1 == v2:
-                curr.next = ListNode(v1)
-                curr.next.next = ListNode(v2)
-                curr = curr.next.next
+        newHead = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = ListNode(l1.val)
                 l1 = l1.next
-                l2 = l2.next
             else:
-                curr.next = ListNode(v1)
-                curr = curr.next
-                l1 = l1.next
-            
+                curr.next = ListNode(l2.val)
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
         return newHead.next
+
+
+#Time complexity: O(N + M) where n & m are the lengths of l1 and l2, Space Complexity: O(M+N) since we are creating a new list that merges both linked lists
+
+# class Solution(object):
+#     def mergeTwoLists(self, l1, l2):
+#         """
+#         :type l1: ListNode
+#         :type l2: ListNode
+#         :rtype: ListNode
+#         """
+#         newHead = ListNode(None)
+#         curr = newHead
+#         while l1 or l2:
+#             if not l1:
+#                 curr.next = l2
+#                 break
+#             if not l2:
+#                 curr.next = l1
+#                 break
+#             v1, v2 = l1.val, l2.val
+            
+#             if v1 > v2:
+#                 curr.next = ListNode(v2)
+#                 curr = curr.next
+#                 l2 = l2.next
+#             elif v1 == v2:
+#                 curr.next = ListNode(v1)
+#                 curr.next.next = ListNode(v2)
+#                 curr = curr.next.next
+#                 l1 = l1.next
+#                 l2 = l2.next
+#             else:
+#                 curr.next = ListNode(v1)
+#                 curr = curr.next
+#                 l1 = l1.next
+            
+#         return newHead.next
