41b7f4dfe9f92ddf59913a1b4964a62bfae72b7c
Anthony <atsui4688@gmail.com>
2020-03-30 00:53:54 -0400

    Question 70 Climbing Stairs

---
+++ LeetCode/Easy/70.Climbing Stairs.py
@@ -0,0 +1,43 @@
+# You are climbing a stair case. It takes n steps to reach to the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# Note: Given n will be a positive integer.
+
+# Example 1:
+
+# Input: 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # if n == 0: return 0
+        # if n == 1: return 1
+        # stairs = [0 for i in range(n)]
+        # stairs[0],stairs[1] = 1, 2
+        # for i in range(2, n):
+        #     stairs[i] = stairs[i-1] + stairs[i-2]
+        # return stairs[-1]
+        prev, curr = 1, 2
+        index =  2
+        while index < n:
+            new = prev + curr
+            prev = curr
+            curr = new
+            index += 1
+        return curr
