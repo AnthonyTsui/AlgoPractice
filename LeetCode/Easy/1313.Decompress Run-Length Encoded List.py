922fa747f61952ec00788710efa5db2466aa7b77
Anthony <atsui4688@gmail.com>
2020-04-23 18:46:44 -0400

    Question 1313. Decompress Run-Length Encoded List

---
+++ LeetCode/Easy/1313.Decompress Run-Length Encoded List.py
@@ -0,0 +1,51 @@
+# We are given a list nums of integers representing a list compressed with run-length encoding.
+
+# Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+# Return the decompressed list.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [2,4,4,4]
+# Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+# The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+# At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+# Example 2:
+
+# Input: nums = [1,1,2,3]
+# Output: [1,3,3]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 100
+# nums.length % 2 == 0
+# 1 <= nums[i] <= 100
+
+
+#Approach: Use an index to traverse the nums list, we jump every two elements
+#We get freq and val using nums[index] and nums[index+1], then just extend our return list
+#with an array of the val*freq
+
+#Time complexity: O(N)
+#Space complexity:O((N/2)*100) since we have at most N/2 freq numbers and they are at most 100
+
+class Solution(object):
+    def decompressRLElist(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        return [x for a, b in zip(nums[0::2], nums[1::2]) for x in [b]*a]
+        
+        # answer = []
+        # index = 0
+        # while index < len(nums):
+        #     freq, val = nums[index], nums[index+1]
+        #     answer.extend([val for i in range(freq)])
+        #     index += 2
+        # return answer
