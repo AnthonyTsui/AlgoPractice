8b1017eca4b80a2c85323281e8f54988a9235439
Anthony <atsui4688@gmail.com>
2020-04-29 18:25:53 -0400

    Question 1046 Last Stone Weight

---
+++ LeetCode/Easy/1046.Last Stone Weight.py
@@ -0,0 +1,44 @@
+# We have a collection of stones, each stone has a positive integer weight.
+
+# Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+# If x == y, both stones are totally destroyed;
+# If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+# At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+ 
+
+# Example 1:
+
+# Input: [2,7,4,1,8,1]
+# Output: 1
+# Explanation: 
+# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+# we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+
+#Approach: use a heap/priority queue  in order to get the two heaviest rocks everytime and continue to maintain our heap as we smash the two heaviest
+#Time Complexity: O(nLogn) where n is the number of elements in stones. Heapify takes O(N) time, and our heap reduces at a nlogn rate 
+#Space complexity: O(N)
+
+import heapq
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        heap = [-weight for weight in stones]
+        heapq.heapify(heap)
+        
+        while len(heap) > 1:
+            x = heapq.heappop(heap)
+            y = heapq.heappop(heap)
+            if x != y:
+                heapq.heappush(heap, x - y)
+            
+        return 0 if not len(heap) else -heap[0]
+        
