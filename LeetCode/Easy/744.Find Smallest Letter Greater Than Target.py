fcbeee506508e2d31750a9fef653c9f9db4d0e5f
Anthony <atsui4688@gmail.com>
2020-05-07 18:04:29 -0400

    Question 83.Remove Duplicates from Sorted List

---
+++ LeetCode/Easy/744.Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,53 @@
+# Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+# Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+# Examples:
+# Input:
+# letters = ["c", "f", "j"]
+# target = "a"
+# Output: "c"
+
+# Input:
+# letters = ["c", "f", "j"]
+# target = "c"
+# Output: "f"
+
+# Input:
+# letters = ["c", "f", "j"]
+# target = "d"
+# Output: "f"
+
+# Input:
+# letters = ["c", "f", "j"]
+# target = "g"
+# Output: "j"
+
+# Input:
+# letters = ["c", "f", "j"]
+# target = "j"
+# Output: "c"
+
+# Input:
+# letters = ["c", "f", "j"]
+# target = "k"
+# Output: "c"
+
+#Time complexity: O(logN), this is just binary search on a sorted list/array
+#Space Complexity: O(1)
+
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left, right = 0, len(letters)-1
+        while left <= right:
+            mid = (left+right) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return letters[0] if left >= len(letters) else letters[left]
---
+++ LeetCode/Easy/83.Remove Duplicates from Sorted List.py
@@ -0,0 +1,33 @@
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# Example 1:
+
+# Input: 1->1->2
+# Output: 1->2
+# Example 2:
+
+# Input: 1->1->2->3->3
+# Output: 1->2->3
+
+#Time Complexity: O(N)
+#Space Complexity: O(1)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head 
+        
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
