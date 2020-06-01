4b1489f063ea9cd4435efe79ac86caa599adc381
Anthony <atsui4688@gmail.com>
2020-06-01 15:14:37 -0400

    Question 219. Contains Duplicate

---
+++ LeetCode/Easy/219.Contains Duplicate II.py
@@ -0,0 +1,31 @@
+# Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+
+#TIme Complexity :O(N)
+#Space Complexity: O(N)
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen:
+                if k >= i - seen[num]:
+                    return True
+            seen[num] = i
+        return False
