aacef841d63b6346bd90d4d7ba980975ad9732bf
Anthony <atsui4688@gmail.com>
2020-06-23 12:35:09 -0400

    Question 540. Single Element in a Sorted Array

---
+++ LeetCode/Medium/540. Single Element in a Sorted Array.py
@@ -0,0 +1,33 @@
+# You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+# Follow up: Your solution should run in O(log n) time and O(1) space.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2,3,3,4,4,8,8]
+# Output: 2
+# Example 2:
+
+# Input: nums = [3,3,7,7,10,11,11]
+# Output: 10
+ 
+
+ #Time Complexity: O(logN)
+ #Space Complexity: O(1)
+
+
+
+from typing import List
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = 2*((left+right)//4)
+            if nums[mid] == nums[mid+1]:
+                left = mid + 2
+            else:
+                right = mid
+        return nums[left]
