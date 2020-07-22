7cc5358604b9e1de7e8dcf734a2a94ba7010f608
Anthony <atsui4688@gmail.com>
2020-07-22 17:59:41 -0400

    JS Question 1221. Split a String in Balanced Strings

---
+++ LeetCode/JS Syntax Practice/1221. Split a String in Balanced Strings.js
@@ -0,0 +1,48 @@
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+// Given a balanced string s split it in the maximum amount of balanced strings.
+
+// Return the maximum amount of splitted balanced strings.
+
+ 
+
+// Example 1:
+
+// Input: s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+// Example 2:
+
+// Input: s = "RLLLLRRRLR"
+// Output: 3
+// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+// Example 3:
+
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
+// Example 4:
+
+// Input: s = "RLRRRLLRLL"
+// Output: 2
+// Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let left, right, ans;
+    left = right = ans = 0;
+    
+    [...s].forEach(function(c){
+        if (c == 'L') { left += 1}
+        else { right += 1}
+        
+        if (left == right){
+            ans += 1
+            left = right = 0
+        }
+    })
+    return ans
+};
